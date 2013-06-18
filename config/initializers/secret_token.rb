f1801d653681111583812d974f008f3a61b7f3b1
Hunter Stewart <hunterdesu@gmail.com>
2013-06-18 15:24:44 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoonBlog::Application.config.secret_token = 'c8c51eb7ce18484edbc1b8bae056ed32ccb79b528237cb9cafcd6c1f3c39a73b274ca23476dcf69cf07ae77734e8bd30663b4cf5e0055edb4684220696031638'
